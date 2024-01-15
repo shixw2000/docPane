7d8e611304768eec336a2dfae970918f6cd3fb89
shixw2000 <shixw_2000@163.com>
2024-01-15 21:43:29 +0800

    delete other files

--- src/my/demo/test/security/TestSecurity.java
+++
@@ -1,9 +0,0 @@
-package my.demo.test.security;
-
-public class TestSecurity {
-	public static void main(String[] args) throws Exception {
-		System.setSecurityManager(new SecurityManager());
-		
-		System.out.println(System.getProperty("java.version"));
-	}
-}
